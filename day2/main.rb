a553e562cc9be06ce868560e199786ef0bf5b96e
Simon Shamoon <simonshm5@gmail.com>
2022-12-05 16:13:56 -0600

    day2

---
+++ day2/main.rb
@@ -0,0 +1,20 @@
+module WonWith
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
+end
+
+
+def solve()
+
+    ans = 0
+    
+    File.readlines("input.txt").each do |line|
+        match = line.split(" ")
+        opponent = match[0]
+        me = match[1]
+
+    end
+
+
+end
